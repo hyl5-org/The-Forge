hyl5-org/The-Forge

commit 86515d3f71610049e9b1cf589c39a9370d4a812c

gltfpack: Implement instanced mesh parsing
Instead of duplicating meshes during parsing, we now parse all meshes[]
elements, fill them with node references and discard empty meshes.

This allows us to implement proper instancing export and is much more
memory efficient on large inputs.

gltf/parsegltf.cpp
@@ -127,15 +127,9 @@ static void fixupIndices(std::vector<unsigned int>& indices, cgltf_primitive_typ
 
 static void parseMeshesGltf(cgltf_data* data, std::vector<Mesh>& meshes)
 {
-	for (size_t ni = 0; ni < data->nodes_count; ++ni)
+	for (size_t mi = 0; mi < data->meshes_count; ++mi)
 	{
-		cgltf_node& node = data->nodes[ni];
-
-		if (!node.mesh)
-			continue;
-
-		const cgltf_mesh& mesh = *node.mesh;
-		int mesh_id = int(&mesh - data->meshes);
+		const cgltf_mesh& mesh = data->meshes[mi];
 
 		for (size_t pi = 0; pi < mesh.primitives_count; ++pi)
 		{
@@ -143,16 +137,13 @@ static void parseMeshesGltf(cgltf_data* data, std::vector<Mesh>& meshes)
 
 			if (primitive.type == cgltf_primitive_type_points && primitive.indices)
 			{
-				fprintf(stderr, "Warning: ignoring primitive %d of mesh %d because indexed points are not supported\n", int(pi), mesh_id);
+				fprintf(stderr, "Warning: ignoring primitive %d of mesh %d because indexed points are not supported\n", int(pi), int(mi));
 				continue;
 			}
 
 			Mesh result = {};
 
-			result.nodes.push_back(&node);
-
 			result.material = primitive.material;
-			result.skin = node.skin;
 
 			result.type = primitive.type;
 
@@ -180,7 +171,7 @@ static void parseMeshesGltf(cgltf_data* data, std::vector<Mesh>& meshes)
 
 				if (attr.type == cgltf_attribute_type_invalid)
 				{
-					fprintf(stderr, "Warning: ignoring unknown attribute %s in primitive %d of mesh %d\n", attr.name, int(pi), mesh_id);
+					fprintf(stderr, "Warning: ignoring unknown attribute %s in primitive %d of mesh %d\n", attr.name, int(pi), int(mi));
 					continue;
 				}
 
@@ -206,7 +197,7 @@ static void parseMeshesGltf(cgltf_data* data, std::vector<Mesh>& meshes)
 
 					if (attr.type == cgltf_attribute_type_invalid)
 					{
-						fprintf(stderr, "Warning: ignoring unknown attribute %s in morph target %d of primitive %d of mesh %d\n", attr.name, int(ti), int(pi), mesh_id);
+						fprintf(stderr, "Warning: ignoring unknown attribute %s in morph target %d of primitive %d of mesh %d\n", attr.name, int(ti), int(pi), int(mi));
 						continue;
 					}
 
@@ -222,6 +213,46 @@ static void parseMeshesGltf(cgltf_data* data, std::vector<Mesh>& meshes)
 			result.target_names.assign(mesh.target_names, mesh.target_names + mesh.target_names_count);
 
 			meshes.push_back(result);
+		}
+	}
+}
+
+static void parseMeshNodesGltf(cgltf_data* data, std::vector<Mesh>& meshes)
+{
+	for (size_t i = 0; i < data->nodes_count; ++i)
+	{
+		cgltf_node& node = data->nodes[i];
+		if (!node.mesh)
+			continue;
+
+		Mesh& mesh = meshes[node.mesh - data->meshes];
+
+		if (mesh.nodes.empty() || mesh.skin == node.skin)
+		{
+			mesh.nodes.push_back(&node);
+			mesh.skin = node.skin;
+		}
+		else
+		{
+			// this should be extremely rare - if the same mesh is used with different skins, we need to duplicate it
+			// in this case we don't spend any effort on keeping the number of duplicates to the minimum, because this
+			// should really never happen.
+			meshes.push_back(mesh);
+
+			meshes.back().nodes.push_back(&node);
+			meshes.back().skin = node.skin;
+		}
+	}
+
+	for (size_t i = 0; i < meshes.size(); ++i)
+	{
+		Mesh& mesh = meshes[i];
+
+		// because the rest of gltfpack assumes that empty nodes array = world-space mesh, we need to filter unused meshes
+		if (mesh.nodes.empty())
+		{
+			mesh.streams.clear();
+			mesh.indices.clear();
 		}
 	}
 }
@@ -336,6 +367,7 @@ cgltf_data* parseGltf(const char* path, std::vector<Mesh>& meshes, std::vector<A
 	}
 
 	parseMeshesGltf(data, meshes);
+	parseMeshNodesGltf(data, meshes);
 	parseAnimationsGltf(data, animations);
 
 	return data;

commit 5be4fd63f14a657ae0d2756b6ce717a00fab30c1

gltfpack: Fix a regression with meshes with multiple primitives
This is a regression from 86515d3f71610049e9b1cf589c39a9370d4a812c. When
parsing mesh primitives, we convert each primitive to a Mesh structure,
and then use node-mesh links to fill nodes[] for each Mesh.

However, we were indexing the mesh primitive array using glTF mesh
index, which is incorrect. We now build a remap table that keeps a range
of Mesh objects for each glTF mesh, and use that to add node mapping to
all meshes in question.

gltf/gltfpack.cpp
@@ -96,7 +96,7 @@ static void printMeshStats(const std::vector<Mesh>& meshes, const char* name)
 		total_draws += std::max(size_t(1), mesh.nodes.size());
 	}
 
-	printf("%s: %d meshes (%d triangles, %d vertices); %d draw calls (%d instances, %lld triangles)\n", name,
+	printf("%s: %d mesh primitives (%d triangles, %d vertices); %d draw calls (%d instances, %lld triangles)\n", name,
 	       int(meshes.size()), int(mesh_triangles), int(mesh_vertices),
 	       int(total_draws), int(total_instances), (long long)total_triangles);
 }

gltf/parsegltf.cpp
@@ -126,7 +126,7 @@ static void fixupIndices(std::vector<unsigned int>& indices, cgltf_primitive_typ
 	}
 }
 
-static void parseMeshesGltf(cgltf_data* data, std::vector<Mesh>& meshes)
+static void parseMeshesGltf(cgltf_data* data, std::vector<Mesh>& meshes, std::vector<std::pair<size_t, size_t> >& mesh_remap)
 {
 	size_t total_primitives = 0;
 
@@ -134,10 +134,13 @@ static void parseMeshesGltf(cgltf_data* data, std::vector<Mesh>& meshes)
 		total_primitives += data->meshes[mi].primitives_count;
 
 	meshes.reserve(total_primitives);
+	mesh_remap.resize(data->meshes_count);
 
 	for (size_t mi = 0; mi < data->meshes_count; ++mi)
 	{
 		const cgltf_mesh& mesh = data->meshes[mi];
+
+		size_t remap_offset = meshes.size();
 
 		for (size_t pi = 0; pi < mesh.primitives_count; ++pi)
 		{
@@ -230,10 +233,12 @@ static void parseMeshesGltf(cgltf_data* data, std::vector<Mesh>& meshes)
 			result.target_weights.assign(mesh.weights, mesh.weights + mesh.weights_count);
 			result.target_names.assign(mesh.target_names, mesh.target_names + mesh.target_names_count);
 		}
+
+		mesh_remap[mi] = std::make_pair(remap_offset, meshes.size());
 	}
 }
 
-static void parseMeshNodesGltf(cgltf_data* data, std::vector<Mesh>& meshes)
+static void parseMeshNodesGltf(cgltf_data* data, std::vector<Mesh>& meshes, const std::vector<std::pair<size_t, size_t> >& mesh_remap)
 {
 	for (size_t i = 0; i < data->nodes_count; ++i)
 	{
@@ -241,22 +246,23 @@ static void parseMeshNodesGltf(cgltf_data* data, std::vector<Mesh>& meshes)
 		if (!node.mesh)
 			continue;
 
-		Mesh& mesh = meshes[node.mesh - data->meshes];
+		std::pair<size_t, size_t> range = mesh_remap[node.mesh - data->meshes];
 
-		if (mesh.nodes.empty() || mesh.skin == node.skin)
+		for (size_t mi = range.first; mi < range.second; ++mi)
 		{
-			mesh.nodes.push_back(&node);
-			mesh.skin = node.skin;
-		}
-		else
-		{
-			// this should be extremely rare - if the same mesh is used with different skins, we need to duplicate it
-			// in this case we don't spend any effort on keeping the number of duplicates to the minimum, because this
-			// should really never happen.
-			meshes.push_back(mesh);
+			Mesh* mesh = &meshes[mi];
 
-			meshes.back().nodes.push_back(&node);
-			meshes.back().skin = node.skin;
+			if (!mesh->nodes.empty() && mesh->skin != node.skin)
+			{
+				// this should be extremely rare - if the same mesh is used with different skins, we need to duplicate it
+				// in this case we don't spend any effort on keeping the number of duplicates to the minimum, because this
+				// should really never happen.
+				meshes.push_back(*mesh);
+				mesh = &meshes.back();
+			}
+
+			mesh->nodes.push_back(&node);
+			mesh->skin = node.skin;
 		}
 	}
 
@@ -483,8 +489,10 @@ cgltf_data* parseGltf(const char* path, std::vector<Mesh>& meshes, std::vector<A
 		return 0;
 	}
 
-	parseMeshesGltf(data, meshes);
-	parseMeshNodesGltf(data, meshes);
+	std::vector<std::pair<size_t, size_t> > mesh_remap;
+
+	parseMeshesGltf(data, meshes, mesh_remap);
+	parseMeshNodesGltf(data, meshes, mesh_remap);
 	parseAnimationsGltf(data, animations);
 
 	bool free_bin = freeUnusedBuffers(data);